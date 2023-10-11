85d62b26d92905d9ffb3eb8dd94003d434376bca
PDMontielC <147265912+PDMontielC@users.noreply.github.com>
2023-10-10 20:31:13 -0600

    Se agrega comentarios

--- UMG_Proyecto_Algoritmos.cpp
+++ UMG_Proyecto_Algoritmos.cpp
@@ -1,8 +1,15 @@
 #Include <iostream>
 
+<<<<<<< Updated upstream
 using namespace std;
 
 main(){
 
     cout<<"Proyecto";
 }
+=======
+    cout<<"Proyecto"<<endl;
+    cout<<"Hola Mundo"<<endl;
+    cout<<"Realizando pruebas"<<endl;
+}
+>>>>>>> Stashed changes
